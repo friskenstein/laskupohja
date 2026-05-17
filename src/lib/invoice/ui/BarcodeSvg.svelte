7533239778b473163fc05feadfce9d352853815a
Johan Frisk <johan@friskenstein.com>
2026-05-17 20:44:27 +0300

    fix: Barcode update issue

--- src/lib/invoice/ui/BarcodeSvg.svelte
+++ src/lib/invoice/ui/BarcodeSvg.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
-	import { onMount } from 'svelte'
 	import JsBarcode from 'jsbarcode'
 
 	export let payload: string
 
 	let element: SVGElement
 
-	onMount(() => {
+	$: if (element) {
 		JsBarcode(element, payload, {
 			format: 'CODE128',
 			displayValue: false,
 			marginRight: 40,
 		})
-	})
+	}
 </script>
 
 <svg bind:this={element} class="w-7/12" data-finnish-bank-barcode-payload={payload}></svg>
